d743a788becb6e3e7261d5bbfcac506f2feefc0c
somethinrother <jamesneilson4@gmail.com>
2019-06-06 20:32:35 -0400

    Improve the logic of exercise 1

--- pe_001.rb
+++ pe_001.rb
@@ -1,5 +1,3 @@
-sum = (1...1000).each_with_object([]) do |num, desired_nums|
-  desired_nums << num if num % 3 == 0 || num % 5 == 0
-end.reduce(:+)
+sum = (1...1000).select() { |num| num % 3 == 0 || num % 5 == 0 }.reduce(:+)
 
 puts sum
