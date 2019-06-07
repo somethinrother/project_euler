369f8d25c944ab016b8ee4d0514e52c939f93b2e
somethinrother <jamesneilson4@gmail.com>
2019-06-06 22:24:47 -0400

    Lay out basic plan

---
+++ pe_004.rb
@@ -0,0 +1,6 @@
+# Define two ranges, each 100 - 999 (all possible 3 digit numbers)
+
+# Increment through the ranges, multiplying both numbers together, and storing
+# the result if it is palindromic
+
+# Find max entry
