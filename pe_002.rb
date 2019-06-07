fed528b85257d88e97a1ecf5dd17adb31b870522
somethinrother <jamesneilson4@gmail.com>
2019-06-06 21:35:40 -0400

    Fibonacci sequencing method accepts a max number to stop there

--- pe_002.rb
+++ pe_002.rb
@@ -1,12 +1,15 @@
 # Create a method that produces array of Fibonacci sequence numbers
-starting_numbers = [1, 2]
-8.times do
-  new_num = starting_numbers[-1] + starting_numbers[-2]
-  starting_numbers << new_num
+def fibonacci_sequence(max_number, first_number, second_number)
+  sequence = [first_number, second_number]
+
+  loop do
+    next_number = sequence[-1] + sequence[-2]
+    return sequence if next_number > max_number
+    sequence << next_number
+  end
 end
 
-puts starting_numbers
-
+puts fibonacci_sequence(700, 1, 2)
 # Filter the results of the Fibonacci array to only even numbers
 
 # Sum the numbers
