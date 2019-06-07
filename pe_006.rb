df5792524fffb4d5997aba5303185c2f7935da60
somethinrother <jamesneilson4@gmail.com>
2019-06-06 23:54:05 -0400

    Method works for range of 10

--- pe_006.rb
+++ pe_006.rb
@@ -3,12 +3,16 @@ def sum_of_each_squared_value_in_range(range)
   values.map { |num| num ** 2 }.reduce(:+)
 end
 
-puts sum_of_each_squared_value_in_range(1..10)
-
 def square_of_the_sum_of_values_in_range(range)
   values = range.to_a
   values.reduce(:+) ** 2
 end
 
-puts square_of_the_sum_of_values_in_range(1..10)
-# Create method to find the difference between the two for the same range
+def difference_between_the_two(range)
+  sum_of_squares = sum_of_each_squared_value_in_range(range)
+  square_of_sum = square_of_the_sum_of_values_in_range(range)
+
+  (sum_of_squares - square_of_sum).abs
+end
+
+puts difference_between_the_two(1..10)
