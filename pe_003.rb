9428056fbe9b5199af85d34e36ff43cb796992f1
somethinrother <jamesneilson4@gmail.com>
2019-06-06 21:48:38 -0400

    Lay out plan

---
+++ pe_003.rb
@@ -0,0 +1,7 @@
+current_prime = 2
+# Define method to find the next prime after current prime
+
+# Define method that accepts the number to be factorized, divides the number by
+# current_prime until it is not a whole number. It then increments the current_prime
+# and starts again. This ends when the number to be factorized, having been
+# divided, is prime itself
