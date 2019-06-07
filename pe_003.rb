1a889d3c6f5592e5d80682357de27ad72305a0a0
somethinrother <jamesneilson4@gmail.com>
2019-06-06 22:05:38 -0400

    Achieve correct result

--- pe_003.rb
+++ pe_003.rb
@@ -12,3 +12,20 @@ end
 # current_prime until it is not a whole number. It then increments the current_prime
 # and starts again. This ends when the number to be factorized, having been
 # divided, is prime itself
+
+number = 600851475143
+
+used_primes = []
+
+loop do
+  loop do
+    break if (number.to_f / current_prime) % 1 != 0
+    number = number.to_f / current_prime
+  end
+
+  used_primes << current_prime
+  break if Prime.prime?(number.to_i) && number % 1 != 0 || number.to_i == 1
+  current_prime = next_prime(current_prime)
+end
+
+puts used_primes.uniq[-1]
