ec53fb2f9b0d202ce0288ec8a4be8ae5aeffb76d
somethinrother <jamesneilson4@gmail.com>
2019-06-06 23:51:56 -0400

    Second method works

--- pe_006.rb
+++ pe_006.rb
@@ -1,4 +1,3 @@
-# Create method to return sum of the squares of all numbers in a range
 def sum_of_each_squared_value_in_range(range)
   values = range.to_a
   values.map { |num| num ** 2 }.reduce(:+)
@@ -6,6 +5,10 @@ end
 
 puts sum_of_each_squared_value_in_range(1..10)
 
-# Create method to return the square of the sum of all number in a range
+def square_of_the_sum_of_values_in_range(range)
+  values = range.to_a
+  values.reduce(:+) ** 2
+end
 
+puts square_of_the_sum_of_values_in_range(1..10)
 # Create method to find the difference between the two for the same range
