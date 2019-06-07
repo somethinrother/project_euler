94468514d2dd40c466fc10da5ac5fe8e7fd90839
somethinrother <jamesneilson4@gmail.com>
2019-06-06 23:50:40 -0400

    First method works

--- pe_006.rb
+++ pe_006.rb
@@ -1,4 +1,10 @@
 # Create method to return sum of the squares of all numbers in a range
+def sum_of_each_squared_value_in_range(range)
+  values = range.to_a
+  values.map { |num| num ** 2 }.reduce(:+)
+end
+
+puts sum_of_each_squared_value_in_range(1..10)
 
 # Create method to return the square of the sum of all number in a range
 
