c3c9dca261b3bea879e35d4f5d2f65071984a842
somethinrother <jamesneilson4@gmail.com>
2019-06-07 14:24:48 -0400

    Reconfigure to a class format, since the method required so many config variables

--- pe_008.rb
+++ pe_008.rb
@@ -3,23 +3,29 @@ require_relative 'classes/numbers'
 # Constants
 NUMBER_BLOCK = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
 
-def highest_product_of_n_adjecent_digits(number, n)
-  max_index = Numbers.digits_in_number(number)
-  numbers = Numbers.number_to_array(number)
-  first_index = 0
-  last_index = n - 1
-  products = []
+class HighestProductFinder
+  attr_reader :max_index, :numbers, :first_index, :last_index
+  attr_accessor :products
 
-  until last_index == max_index do
-    product = numbers[first_index..last_index].reduce(:*)
-    products << product unless product.zero?
-    first_index += 1
-    last_index += 1
+  def initialize(number, adjacent_digits)
+    @max_index = Numbers.digits_in_number(number)
+    @numbers = Numbers.number_to_array(number)
+    @first_index = 0
+    @last_index = adjacent_digits - 1
+    @products = []
   end
 
-  products.max
+  def highest_product_of_n_adjecent_digits
+    until @last_index == @max_index do
+      product = @numbers[@first_index..@last_index].reduce(:*)
+      @products << product unless product.zero?
+      @first_index += 1
+      @last_index += 1
+    end
+
+    @products.max
+  end
 end
 
-
-
-puts highest_product_of_n_adjecent_digits(NUMBER_BLOCK, 13)
+finder = HighestProductFinder.new(NUMBER_BLOCK, 13)
+puts finder.highest_product_of_n_adjecent_digits
