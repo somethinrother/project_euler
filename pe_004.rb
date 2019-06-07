3ea2cfe6c268e966cba698cc809f7207b24c0b3f
somethinrother <jamesneilson4@gmail.com>
2019-06-06 22:34:22 -0400

    A little cleaner

--- pe_004.rb
+++ pe_004.rb
@@ -1,12 +1,10 @@
 first_multipliers, second_multipliers = (100..999), (100..999)
 
-palindromes = []
-
-first_multipliers.each do |first_multiplier|
+palindromes = first_multipliers.each_with_object([]) do |first_multiplier, palindrome_array|
   second_multipliers.each do |second_multiplier|
     product = first_multiplier * second_multiplier
-    palindromes << product if product.to_s == product.to_s.reverse
+    palindrome_array << product if product.to_s == product.to_s.reverse
   end
-end
+end.max
 
-puts palindromes.max
+puts palindromes
