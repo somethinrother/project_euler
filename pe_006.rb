cd573118420ec2b736417821ea66d1f991ec1667
somethinrother <jamesneilson4@gmail.com>
2019-06-06 23:47:20 -0400

    Create gameplan

---
+++ pe_006.rb
@@ -0,0 +1,5 @@
+# Create method to return sum of the squares of all numbers in a range
+
+# Create method to return the square of the sum of all number in a range
+
+# Create method to find the difference between the two for the same range
