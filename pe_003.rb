1bd0320b7e55d446baa452a598459e2c6c647b4e
somethinrother <jamesneilson4@gmail.com>
2019-06-06 23:06:24 -0400

    Bring back is_whole method

--- pe_003.rb
+++ pe_003.rb
@@ -6,18 +6,22 @@ def next_prime(current_prime)
   current_prime
 end
 
+def is_whole(number)
+  number % 1 == 0
+end
+
 def max_prime_factor(number)
   current_prime = 2
 
   loop do
     loop do
       # Break if the upcoming division would result in a number with a remainder
-      break if (number.to_f / current_prime) % 1 != 0
+      break if !is_whole(number.to_f / current_prime)
       number = number.to_f / current_prime
     end
 
     # If the number is prime, then it is the largest prime factor
-    is_prime = Prime.prime?(number.to_i) && number % 1 == 0
+    is_prime = Prime.prime?(number.to_i) && is_whole(number)
     return number.to_i if is_prime
     current_prime = next_prime(current_prime)
   end
