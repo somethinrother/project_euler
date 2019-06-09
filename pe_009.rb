3e603ae292866e3c37f96915862caa5513fa6367
somethinrother <jamesneilson4@gmail.com>
2019-06-09 11:09:00 -0400

    Extract logic into reusable method

--- pe_009.rb
+++ pe_009.rb
@@ -7,16 +7,20 @@
 
 # Plan
 # Find all permutations of 3 number between 1 and 1000 where the numbers are consecutive
-range = (1..1000).to_a
-consecutive_combinations = range.each_cons(3).to_a
-combinations = consecutive_combinations.map do |combination|
-  {
-    a: combination[0],
-    b: combination[1],
-    c: combination[2]
-  }
+def consecutive_combinations_of_n_length_within_range(range_size, combination_length)
+  range = (1..range_size).to_a
+  consecutive_combinations = range.each_cons(combination_length).to_a
+  consecutive_combinations.map do |combination|
+    {
+      a: combination[0],
+      b: combination[1],
+      c: combination[2]
+    }
+  end
 end
 
+puts consecutive_combinations_of_n_length_within_range(1000, 3)
 # Now the result of that down to only options where a + b + c = 1000
+
 # Narrow that array down to only the entries where a2 + b2 = c2
 # Find the product of that abc
