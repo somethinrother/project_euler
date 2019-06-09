07cb4f59b8d9167ca6f34c2948181127927ca01f
somethinrother <jamesneilson4@gmail.com>
2019-06-09 14:32:11 -0400

    Run migration to create problems

--- project_euler/db/migrate/20190609182701_create_problems.rb
+++ project_euler/db/migrate/20190609182701_create_problems.rb
@@ -1,6 +1,9 @@
 class CreateProblems < ActiveRecord::Migration[5.2]
   def change
     create_table :problems do |t|
+      t.string :name
+      t.string :link
+      t.string :solution
 
       t.timestamps
     end
--- project_euler/db/schema.rb
+++ project_euler/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_09_182701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "problems", force: :cascade do |t|
+    t.string "name"
+    t.string "link"
+    t.string "solution"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
