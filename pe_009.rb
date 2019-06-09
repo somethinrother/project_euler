262d078fbc8dd46edfc4846b638c0972b110a90a
somethinrother <jamesneilson4@gmail.com>
2019-06-09 11:05:23 -0400

    Get the data in a format where the sides are labelled

--- pe_009.rb
+++ pe_009.rb
@@ -7,6 +7,16 @@
 
 # Plan
 # Find all permutations of 3 number between 1 and 1000 where the numbers are consecutive
+range = (1..1000).to_a
+consecutive_combinations = range.each_cons(3).to_a
+combinations = consecutive_combinations.map do |combination|
+  {
+    a: combination[0],
+    b: combination[1],
+    c: combination[2]
+  }
+end
+
 # Now the result of that down to only options where a + b + c = 1000
 # Narrow that array down to only the entries where a2 + b2 = c2
 # Find the product of that abc
