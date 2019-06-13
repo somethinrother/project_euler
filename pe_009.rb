4571e00f1b766c1788daee48536b747b8c54ac4f
somethinrother <jamesneilson4@gmail.com>
2019-06-13 19:57:26 -0400

    Previous plan was too cpu intensive. Laid out new plan

--- pe_009.rb
+++ pe_009.rb
@@ -5,33 +5,9 @@
   # representing a, b, and c
   # e.g. { a: 3, b: 4, c: 5 }
 
-# Plan
-# Find all permutations of 3 number between 1 and 1000 where the numbers are greater than each other in order
-# range = (1..1000).to_a
-# permutations = range.permutation(3).to_a
-# formatted_permutations = permutations.map { |p| { a: p[0], b: p[1], c: p[2] } }
-# possible_triplets = formatted_permutations.select { |p| p[:a] < p[:b] && p[:b] < p[:c] }
-
-# puts possible_triplets
-# Now the result of that down to only options where a + b + c = 1000
-
-# Narrow that array down to only the entries where a2 + b2 = c2
-# Find the product of that abc
-
-def subsets(n, max_k)
-  results = []
-  1.upto(max_k) {|k| results << permutations(n,k, results.last)}
-  results
-end
-
-def permutations(n,k,previous_iteration=nil)
-  return (1...n).collect {|x| [x]} if k == 1
-
-  previous_iteration ||= permutations(n,k-1)
-  (1...n).inject([]) do |result, to_add|
-    result.concat( previous_iteration.inject([]) do |memo, perm|
-      memo << (perm + [to_add]).sort unless perm.include?(to_add)
-      memo
-    end)
-  end.uniq
-end
+# Loop over numbers from 1 to max (1000) This is our a.
+# For each number in the loop, we must also check numbers from a to 1000
+# (this is acceptable, as we will have tested the lower numbered combo already)
+# This number is our b.
+# c is 1000 - (a+b).
+# From this 3-way combination, we check if it is a pythagorean triplet
