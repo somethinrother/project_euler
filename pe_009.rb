6fd1f13cd25a6da9e1e661aa3b5940ff4c1ad96f
somethinrother <32532809+somethinrother@users.noreply.github.com>
2019-06-13 20:06:00 -0400

    Pe 009 (#11)
    * Create empty file for problem 9
    
    * Set out gameplan in file
    
    * Get the data in a format where the sides are labelled
    
    * Extract logic into reusable method
    
    * Find permutation manual method here: https://www.ruby-forum.com/t/how-to-quickly-make-permutations/141819/3, might be useful for widekiq integration to speed up processing
    
    * Previous plan was too cpu intensive. Laid out new plan
    
    * The check for a pythagorean triplet works
    
    * Get method working

---
+++ pe_009.rb
@@ -0,0 +1,33 @@
+# https://projecteuler.net/problem=9
+
+# Criteria:
+  # The result must be a hash containing three consecutive numbers,
+  # representing a, b, and c
+  # e.g. { a: 3, b: 4, c: 5 }
+
+# Loop over numbers from 1 to max (1000) This is our a.
+# For each number in the loop, we must also check numbers from a to 1000
+# (this is acceptable, as we will have tested the lower numbered combo already)
+# This number is our b.
+# c is 1000 - (a+b).
+# From this 3-way combination, we check if it is a pythagorean triplet
+
+def is_a_right_angle_triangle?(a, b, c)
+  a**2 + b**2 == c**2
+end
+
+def is_a_triplet?(a, b, c)
+  c > b && b > a
+end
+
+def is_pythagorean_triplet?(a, b, c)
+  is_a_right_angle_triangle?(a, b, c) && is_a_triplet?(a, b, c)
+end
+
+(1..1000).each do |a|
+  (a..1000).each do |b|
+    c = 1000 - (a + b)
+    product = a * b * c
+    puts product if is_pythagorean_triplet?(a, b, c)
+  end
+end
