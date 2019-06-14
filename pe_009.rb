56e0cc6614c1a6f9e5d530ea08e024e4875a7edf
somethinrother <jamesneilson4@gmail.com>
2019-06-13 20:10:06 -0400

    Remove comments from #9

--- pe_009.rb
+++ pe_009.rb
@@ -1,17 +1,5 @@
 # https://projecteuler.net/problem=9
 
-# Criteria:
-  # The result must be a hash containing three consecutive numbers,
-  # representing a, b, and c
-  # e.g. { a: 3, b: 4, c: 5 }
-
-# Loop over numbers from 1 to max (1000) This is our a.
-# For each number in the loop, we must also check numbers from a to 1000
-# (this is acceptable, as we will have tested the lower numbered combo already)
-# This number is our b.
-# c is 1000 - (a+b).
-# From this 3-way combination, we check if it is a pythagorean triplet
-
 def is_a_right_angle_triangle?(a, b, c)
   a**2 + b**2 == c**2
 end
--- pe_010.rb
+++ pe_010.rb
@@ -1,4 +1,8 @@
 # https://projecteuler.net/problem=10
 
+require_relative 'classes/numbers'
+
 # For all numbers from 1 - 2000000, check if the number is prime, filter out if
 # not. Sum what remains.
+
+(1..2_000_000).select
