286e4c80b633207ef0449a249625601222349f69
somethinrother <jamesneilson4@gmail.com>
2019-06-13 20:04:53 -0400

    Get method working

--- pe_009.rb
+++ pe_009.rb
@@ -23,3 +23,11 @@ end
 def is_pythagorean_triplet?(a, b, c)
   is_a_right_angle_triangle?(a, b, c) && is_a_triplet?(a, b, c)
 end
+
+(1..1000).each do |a|
+  (a..1000).each do |b|
+    c = 1000 - (a + b)
+    product = a * b * c
+    puts product if is_pythagorean_triplet?(a, b, c)
+  end
+end
