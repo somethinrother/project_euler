9f53fe8768fdb29ecacace9979b652de53ead782
somethinrother <jamesneilson4@gmail.com>
2019-06-13 20:12:03 -0400

    Remove comment

--- pe_010.rb
+++ pe_010.rb
@@ -2,7 +2,4 @@
 
 require_relative 'classes/numbers'
 
-# For all numbers from 1 - 2000000, check if the number is prime, filter out if
-# not. Sum what remains.
-
 puts (1..2_000_000).select { |num| Numbers.prime?(num) }.reduce(:+)
