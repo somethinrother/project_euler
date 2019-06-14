ca98791e5ca4e95dc9df510f24ddce7ba4e13239
somethinrother <jamesneilson4@gmail.com>
2019-06-13 20:01:30 -0400

    The check for a pythagorean triplet works

--- pe_009.rb
+++ pe_009.rb
@@ -11,3 +11,15 @@
 # This number is our b.
 # c is 1000 - (a+b).
 # From this 3-way combination, we check if it is a pythagorean triplet
+
+def is_a_right_angle_triangle?(a, b, c)
+  a**2 + b**2 == c**2
+end
+
+def is_a_triplet?(a, b, c)
+  c > b && b > a
+end
+
+def is_pythagorean_triplet?(a, b, c)
+  is_a_right_angle_triangle?(a, b, c) && is_a_triplet?(a, b, c)
+end
